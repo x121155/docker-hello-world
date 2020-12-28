8fe49ad111aa1a506adeb9eae5608040e9a2c2b9
x121155 <x121155@hotmail.com>
2020-12-29 01:22:55 +0800

    a webhook test

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:12.16.3
+
+WORKDIR /code
+
+ARG PORT=80
+ENV PORT $PORT
+
+COPY package.json /code/package.json
+
+RUN npm install
+
+COPY . /code
+
+CMD [ "node", "src/server.js" ]
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+const express 	= require( 'express' )
+const appRouter = express.Router()
+
+appRouter.get( '/*', function( req, res ) {
+  res.send( '<h1>Hello, my   DockerCon!</h1>' )
+})
+
+module.exports = appRouter
